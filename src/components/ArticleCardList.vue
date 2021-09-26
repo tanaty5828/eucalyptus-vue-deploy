ef326ac66776783a43e58fd1519cedead4b2c4b1
hirokiT4N4K4 <mtpd7700@gmail.com>
2021-09-26 15:29:40 +0900

    component Article Card List

---
+++ src/components/ArticleCardList.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-flex offset-xs3 xs6>
+      <template v-for="article in articles">
+        <v-card :key="article.id" :loading="loading" class="mx-auto my-12">
+          <v-card-title>{{ article.title }}</v-card-title>
+          <v-divider dark="true" class="mx-4"></v-divider>
+          <v-card-text>{{ article.content | sliceContent }}</v-card-text>
+          <v-btn text color="deep-purple accent-4" class="mx-1 mb-2"
+            >Read More</v-btn
+          >
+        </v-card>
+      </template>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    articles: {
+      type: Object,
+      default: {},
+      required: true,
+    },
+  },
+  filters: {
+    sliceContent(content) {
+      if (!content) return "";
+      if (content.length > 200) {
+        return content.slice(0, 200) + "...";
+      }
+      return content;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,14 @@
 <template>
-  <v-container>
-    <v-flex offset-xs3 xs6>
-      <template v-for="article in articles">
-        <v-card :key="article.id" :loading="loading" class="mx-auto my-12">
-          <v-card-title>{{ article.title }}</v-card-title>
-          <v-divider dark="true" class="mx-4"></v-divider>
-          <v-card-text>{{ article.content | sliceContent }}</v-card-text>
-          <v-btn text color="deep-purple accent-4" class="mx-1 mb-2"
-            >Read More</v-btn
-          >
-        </v-card>
-      </template>
-    </v-flex>
-  </v-container>
+  <article-card-list :articles="articles"></article-card-list>
 </template>
 
 <script>
+import ArticleCardList from '../components/ArticleCardList.vue'
 import axios from "axios";
 export default {
+  components: {
+    ArticleCardList
+  },
   data() {
     return {
       articles: {},
@@ -25,15 +16,6 @@ export default {
   },
   created() {
     this.getArticles();
-  },
-  filters: {
-    sliceContent(content) {
-      if (!content) return "";
-      if (content.length > 200) {
-        return content.slice(0, 200) + "...";
-      }
-      return content;
-    },
   },
   methods: {
     getArticles() {
