f505441d3108a8729656fa7dc1325a88f41b0ce0
hirokiT4N4K4 <mtpd7700@gmail.com>
2021-09-26 16:40:44 +0900

    display card

--- src/components/ArticleCardList.vue
+++ src/components/ArticleCardList.vue
@@ -4,7 +4,9 @@
       <v-card :key="article.id" :loading="loading" class="mx-auto my-12">
         <v-card-title>{{ article.title }}</v-card-title>
         <v-divider dark="true" class="mx-4"></v-divider>
-        <v-card-text>{{ article.content | sliceContent }}</v-card-text>
+        <!-- <v-card-text>
+          <div v-html="markedContent(article.content)"></div>
+        </v-card-text> -->
         <v-btn text color="deep-purple accent-4" class="mx-1 mb-2"
           >Read More</v-btn
         >
@@ -14,6 +16,7 @@
 </template>
 
 <script>
+import marked from "marked";
 export default {
   props: {
     articles: {
@@ -21,14 +24,18 @@ export default {
       required: true,
     },
   },
-  filters: {
-    sliceContent(content) {
-      if (!content) return "";
-      if (content.length > 200) {
-        return content.slice(0, 200) + "...";
-      }
-      return content;
-    },
-  },
+  methods: {
+      markedContent(markdown){
+          return marked(markdown)
+      },
+        sliceContent(content) {
+        if (!content) return "";
+        if (content.length > 200) {
+            console.log(content);
+            return content.slice(0, 200) + "...";
+        }
+        return content;
+        },
+  }
 };
 </script>
