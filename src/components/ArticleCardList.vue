2440899475815b036f1b98e8a23fb523795aef35
hirokiT4N4K4 <mtpd7700@gmail.com>
2021-09-26 16:45:08 +0900

    modify error

--- src/components/ArticleCardList.vue
+++ src/components/ArticleCardList.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <template v-for="article in articles">
-      <v-card :key="article.id" :loading="loading" class="mx-auto my-12">
+      <v-card :key="article.id" class="mx-auto my-12">
         <v-card-title>{{ article.title }}</v-card-title>
-        <v-divider dark="true" class="mx-4"></v-divider>
+        <v-divider class="mx-4"></v-divider>
+        <!-- <v-divider :dark=true class="mx-4"></v-divider> -->
         <!-- <v-card-text>
           <div v-html="markedContent(article.content)"></div>
         </v-card-text> -->
@@ -20,7 +21,7 @@ import marked from "marked";
 export default {
   props: {
     articles: {
-      type: Object,
+      type: Array,
       required: true,
     },
   },
