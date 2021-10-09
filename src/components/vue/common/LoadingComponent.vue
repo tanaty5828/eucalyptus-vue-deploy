81619ed37b52d4ab4468cf5aecc2b738df78db3b
DaisukeHirabayashi <50356043+DaisukeHirabayashi@users.noreply.github.com>
2021-10-09 16:12:25 +0900

    close delayの削除

--- src/components/vue/common/LoadingComponent.vue
+++ src/components/vue/common/LoadingComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="value" max-width="290" persistent overlay-opacity="0.5" close-delay="3000">
+    <v-dialog v-model="value" max-width="290" persistent overlay-opacity="0.5">
       <v-card class="dialog">
         <v-card-text class="pt-6">
           <v-row justify="center">
