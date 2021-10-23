61063a8a27324a6c7b9661b8e381754a86f98a26
DaisukeHirabayashi <daisuke@pluslab.org>
2021-10-23 22:24:56 +0900

    記事のページ遷移をhomeのページにて追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,6 +16,38 @@
     <v-flex offset-md2 md8 offset-xs1 xs10>
       <article-card-list :articles="articles"></article-card-list>
     </v-flex>
+    <v-card
+      flat
+    >
+      <v-card-text>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-btn-toggle
+            tile
+            color="green darken-4"
+            group
+          >
+          <v-btn value=1 @click="movePage(1)">
+            1
+          </v-btn>
+
+          <v-btn value=2 @click="movePage(2)">
+            2
+          </v-btn>
+
+          <v-btn value=3 @click="movePage(3)">
+            3
+          </v-btn>
+
+          <v-btn value=4 @click="movePage(4)">
+            4
+          </v-btn>
+          </v-btn-toggle>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
@@ -64,6 +96,9 @@ export default {
           console.log(error);
           this.is_loading = false;
         });
+    },
+    movePage(page){
+      this.$router.push("/articles?page=" + page).catch(()=>{});
     },
   },
 };
