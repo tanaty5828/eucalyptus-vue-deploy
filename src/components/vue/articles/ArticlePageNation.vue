1b2820b454abb15b24581d760ce37e7964ab73de
DaisukeHirabayashi <daisuke@pluslab.org>
2021-10-24 17:56:27 +0900

    pagenationのコンポーネント化

---
+++ src/components/vue/articles/ArticlePageNation.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card flat>
+    <v-card-text>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-btn-toggle
+          tile
+          color="green darken-4"
+          group
+        >
+        <v-btn value=1 @click="movePage(1)">
+          1
+        </v-btn>
+
+        <v-btn value=2 @click="movePage(2)">
+          2
+        </v-btn>
+
+        <v-btn value=3 @click="movePage(3)">
+          3
+        </v-btn>
+
+        <v-btn value=4 @click="movePage(4)">
+          4
+        </v-btn>
+        </v-btn-toggle>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  methods: {
+    movePage(page){
+      this.$router.push("/articles?page=" + page).catch(()=>{});
+    },
+  },
+};
+</script>
--- src/views/articles/Index.vue
+++ src/views/articles/Index.vue
@@ -4,49 +4,21 @@
     <v-flex offset-md2 md8 offset-xs1 xs10>
       <article-card-list :articles="articles"></article-card-list>
     </v-flex>
-    <v-card
-      flat
-    >
-      <v-card-text>
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-btn-toggle
-            tile
-            color="green darken-4"
-            group
-          >
-          <v-btn value=1 @click="movePage(1)">
-            1
-          </v-btn>
-
-          <v-btn value=2 @click="movePage(2)">
-            2
-          </v-btn>
-
-          <v-btn value=3 @click="movePage(3)">
-            3
-          </v-btn>
-
-          <v-btn value=4 @click="movePage(4)">
-            4
-          </v-btn>
-          </v-btn-toggle>
-        </v-row>
-      </v-card-text>
-    </v-card>
+    <article-page-nation />
   </v-container>
 </template>
 
 <script>
 import ArticleCardList from "../../components/vue/articles/ArticleCardList.vue";
+import ArticlePageNation from "../../components/vue/articles/ArticlePageNation.vue";
 import LoadingComponent from "../../components/vue/common/LoadingComponent.vue";
+
 import scroll from "../../components/js/common/scroll.js";
 import axios from "axios";
 export default {
   components: {
     ArticleCardList,
+    ArticlePageNation,
     LoadingComponent,
   },
   data() {
@@ -76,9 +48,6 @@ export default {
           console.log(error);
           this.is_loading = false;
         });
-    },
-    movePage(page){
-      this.$router.push("/articles?page=" + page).catch(()=>{});
     },
   },
 };
